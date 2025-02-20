11add34c4b379e8707c49092570d2e6e5ff937d0
Rifat Fauzan <rifat.fauzan8@gmail.com>
2025-02-21 01:13:17 +0700

    feat: Fix deployment

--- sitrack-frontend/vite.config.ts
+++ sitrack-frontend/vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
   },
   preview: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT || '4173')
+    port: parseInt(process.env.PORT || '4173'),
+    allowedHosts: ['sitrack-fe.up.railway.app']
   }
 })
