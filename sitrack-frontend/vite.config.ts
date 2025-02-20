74928c058eff6115b6af188beb2b0d7ee70eea27
Rifat Fauzan <rifat.fauzan8@gmail.com>
2025-02-21 01:03:25 +0700

    feat: Add port buat deployment

--- sitrack-frontend/vite.config.ts
+++ sitrack-frontend/vite.config.ts
@@ -15,4 +15,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT || '4173')
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT || '4173')
+  }
 })
