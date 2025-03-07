81c23588492f0115579ade44acbafc305009460b
Rifat Fauzan <rifat.fauzan8@gmail.com>
2025-03-08 00:11:19 +0700

    feat: Added sorting and search for datatable

--- sitrack-frontend/src/views/UserListView.vue
+++ sitrack-frontend/src/views/UserListView.vue
@@ -1,61 +1,91 @@
 <template>
-    <div class="flex h-screen">
-        <Sidebar />
-
-        <div class="flex-1 flex flex-col min-h-screen">
-            <HeaderComponent title="List Pengguna" />
-            <div class="flex-1 p-4 main-content overflow-auto">
-                <div class="container mx-auto max-w-4xl">
-                    <div class="flex justify-between items-center mb-4">
-                        <span class="p-input-icon-left">
-                            <InputText v-model="filters['global'].value" placeholder="Search Pengguna" />
-                        </span>
-                        <VButton 
-                            title="+ Buat" 
-                            class="bg-[#1C5D99] text-white px-4 py-2 rounded" 
-                            @click="() => $router.push('/users/create')" 
-                        />
-                    </div>
-
-                    <DataTable 
-                        :value="userStore.users" 
-                        v-model:filters="filters"
-                        paginator 
-                        :rows="10" 
-                        :rowsPerPageOptions="[5, 10, 20]" 
-                        dataKey="id"
-                        :globalFilterFields="['username', 'role']"
-                        stripedRows
-                        tableStyle="min-width: 50rem"
-                        paginatorTemplate="RowsPerPageDropdown PrevPageLink CurrentPageReport NextPageLink"
-                        currentPageReportTemplate="{first} to {last}"
-                        class="custom-datatable"
-                    >
-                        <template #empty>No users found.</template>
-                        <template #loading>Loading users data. Please wait.</template>
-                        
-                        <Column header="No." style="width: 5%">
-                            <template #body="{ index }">
-                                <div class="text-center">{{ index + 1 }}</div>
-                            </template>
-                        </Column>
-                        
-                        <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header"></Column>
-                        
-                        <Column style="width: 10%;">
-                            <template #body="slotProps">
-                                <div class="flex justify-center">
-                                    <VButton title="Edit" class="bg-[#1C5D99] text-white px-4 py-2 rounded"
-                                         @click="() => $router.push({ name: 'edit users', query: { id: slotProps.data.id } })" />
-                                </div>
-                            </template>
-                        </Column>
-                    </DataTable>
-                </div>
+  <div class="flex h-screen">
+    <Sidebar />
+    <div class="flex-1 flex flex-col min-h-screen">
+      <HeaderComponent title="List Pengguna" />
+      <div class="flex-1 p-4 main-content overflow-auto">
+        <div class="container mx-auto max-w-4xl">
+          <div class="card">
+            <div class="flex justify-between items-center mb-4">
+              <span class="p-input-icon-left">
+                <InputText v-model="filters['global'].value" placeholder="Search Keyword..." />
+              </span>
+              <VButton
+                title="+ Buat"
+                class="bg-[#1C5D99] text-white px-4 py-2 rounded"
+                @click="() => $router.push('/users/create')"
+              />
             </div>
-            <FooterComponent />
+            <DataTable
+              v-model:filters="filters"
+              :value="userStore.users"
+              paginator
+              :rows="10"
+              dataKey="id"
+              filterDisplay="row"
+              :loading="loading"
+              :globalFilterFields="['username', 'role']"
+              stripedRows
+              tableStyle="min-width: 50rem"
+              paginatorTemplate="PrevPageLink CurrentPageReport NextPageLink"
+              currentPageReportTemplate="{first} to {last} of {totalRecords} users"
+              class="custom-datatable"
+            >
+              <template #empty> No users found. </template>
+              <template #loading> Loading user data. Please wait. </template>
+
+              <Column header="No." style="width: 5%">
+                <template #body="{ index }">
+                  <div class="text-center">{{ index + 1 }}</div>
+                </template>
+              </Column>
+
+              <Column field="username" header="Username" class="p-sortable-column" style="min-width: 25%" sortable>
+                <template #body="{ data }">
+                  {{ data.username }}
+                </template>
+                <template #filter="{ filterModel, filterCallback }">
+                  <InputText
+                    v-model="filterModel.value"
+                    type="text"
+                    @input="filterCallback()"
+                    placeholder="Search by username..."
+                  />
+                </template>
+              </Column>
+
+              <Column field="role" header="Role" style="min-width: 12rem" sortable>
+                <template #body="{ data }">
+                  {{ data.role }}
+                </template>
+                <template #filter="{ filterModel, filterCallback }">
+                  <InputText
+                    v-model="filterModel.value"
+                    type="text"
+                    @input="filterCallback()"
+                    placeholder="Search by role..."
+                  />
+                </template>
+              </Column>
+
+              <Column header="Actions" style="width: 10%">
+                <template #body="slotProps">
+                  <div class="flex justify-center">
+                    <VButton
+                      title="Edit"
+                      class="bg-[#1C5D99] text-white px-4 py-2 rounded"
+                      @click="() => $router.push({ name: 'edit users', query: { id: slotProps.data.id } })"
+                    />
+                  </div>
+                </template>
+              </Column>
+            </DataTable>
+          </div>
         </div>
+      </div>
+      <FooterComponent />
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -68,40 +98,26 @@ import { onMounted, ref } from 'vue';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import InputText from 'primevue/inputtext';
-import { FilterMatchMode, FilterOperator } from '@primevue/core/api';
-import { useRouter } from 'vue-router';
+import { FilterMatchMode } from '@primevue/core/api';
 
 const userStore = useUserStore();
+const loading = ref(true);
 const filters = ref({
-    global: { value: null, matchMode: FilterMatchMode.CONTAINS }
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  username: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+  role: { value: null, matchMode: FilterMatchMode.STARTS_WITH }
 });
 
-const columns = ref([
-    { field: 'username', header: 'Username' },
-    { field: 'role', header: 'Role' }
-]);
-
 onMounted(async () => {
-    await userStore.fetchUsers();
+  await userStore.fetchUsers();
+  loading.value = false;
 });
 </script>
 
 <style scoped>
-.p-inputtext {
-    border: 1px solid #d1d5db;
-    border-radius: 0.375rem;
-    padding: 0.5rem;
-}
-
-.p-input-icon-left > i {
-    position: absolute;
-    left: 0.5rem;
-    top: 50%;
-    transform: translateY(-50%);
-    pointer-events: none;
-}
-
-.p-input-icon-left > .p-inputtext {
-    padding-left: 2rem;
+:deep(.p-inputtext) {
+  border: 1px solid #d1d5db;
+  border-radius: 0.375rem;
+  padding: 0.5rem;
 }
 </style>
