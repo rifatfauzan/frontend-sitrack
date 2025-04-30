1ea3f24ae9d4288969b70c1cf0523a26d040346a
Rifat Fauzan <rifat.fauzan8@gmail.com>
2025-04-30 22:59:58 +0700

    feat: Fix sidebar lagi

--- sitrack-frontend/src/components/vSidebar.vue
+++ sitrack-frontend/src/components/vSidebar.vue
@@ -105,7 +105,7 @@
                         </li>
                         <li>
                             <router-link to="/request-assets" class="flex items-center gap-4 cursor-pointer text-lg sidebar-item">
-                                <i class="fas fa-file-signature"></i>
+                                <i class="fas fa-file-signature text-xs"></i>
                                 <span>Request Asset</span>
                             </router-link>
                         </li>
