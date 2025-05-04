6d3f622c1daf595899a382ebd345fecead180679
azmifal <kunazmifalah@gmail.com>
2025-05-04 18:18:17 +0700

    fix: adding filter status for spj and order

--- sitrack-frontend/src/views/requestAsset/RequestAssetListView.vue
+++ sitrack-frontend/src/views/requestAsset/RequestAssetListView.vue
@@ -74,18 +74,17 @@ const filteredData = () => {
         <div class="container mx-auto max-w-7xl">
           <div class="card">
             <div class="flex justify-between items-center mb-4">
-              <span class="p-input-icon-left">
-                <InputText v-model="filters.global.value" placeholder="Search Request Asset..." class="w-64" />
-              </span>
-
-              <Dropdown
-                v-model="selectedStatus"
-                :options="statusOptions"
-                optionLabel="label"
-                optionValue="value"
-                placeholder="Filter Status"
-                class="w-60"
-              />
+              <div class="flex items-center gap-4">
+                  <InputText v-model="filters.global.value" placeholder="Search Request Asset..." />
+                  <Dropdown
+                    v-model="selectedStatus"
+                    :options="statusOptions"
+                    optionLabel="label"
+                    optionValue="value"
+                    placeholder="Filter Status"
+                    class="w-60"
+                  />
+                </div>
 
               <VButton title="+ Buat" class="bg-[#1C5D99] text-white px-4 py-2 rounded" @click="() => router.push('/request-assets/create')" />
             </div>
